74951d2e69127561998712204d1815ba4ae9b3f2
keerthi110504 <keerthimuthyala151@gmail.com>
2026-03-13 14:03:36 +0530

    changes

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,8 +1,12 @@
 import { useContext, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { AppContext } from "../App";
+import axios from "axios";
 function Cart() {
-  const { cart, setCart } = useContext(AppContext);
+  const { cart, setCart, user } = useContext(AppContext);
   const [orderValue, setOrderValue] = useState(0);
+  const API_URL = import.meta.env.VITE_API_URL;
+  const Navigate = useNavigate();
   const increment = (id) => {
     setCart(
       cart.map((item) => {
@@ -34,25 +38,48 @@ function Cart() {
     );
   }, [cart]);
 
+  const placeOrder = async () => {
+    if (user?.email) {
+      const url = `${API_URL}/orders`;
+      const order = {
+        email: user.email,
+        items: cart,
+        orderValue: orderValue,
+        orderDate: Date.now(),
+      };
+      console.log(url, order);
+      const response = await axios.post(url, order, {
+        headers: { Authorization: `Bearer ${user.token}` },
+      });
+      setCart([]);
+      Navigate("/orders");
+    }
+  };
+
   return (
     <div>
       <h1>My Cart</h1>
       <ol>
-        {cart.map((item) => (
-          <li key={item._id}>
-            {item.name}-{item.price}-
-            <button onClick={() => decrement(item._id)}>-</button>
-            {item.quantity}
-            <button onClick={() => increment(item._id)}>+</button>-
-            {item.quantity * item.price}
-          </li>
-        ))}
+        {cart &&
+          cart.map((item) => (
+            <li key={item._id}>
+              {item.name}-{item.price}-
+              <button onClick={() => decrement(item._id)}>-</button>
+              {item.quantity}
+              <button onClick={() => increment(item._id)}>+</button>-
+              {item.quantity * item.price}
+            </li>
+          ))}
       </ol>
       <p>
         <strong>Order Value:{orderValue}</strong>
       </p>
       <p>
-        <button>Place Order</button>
+        {user?.email ? (
+          <button onClick={placeOrder}>Place Order</button>
+        ) : (
+          <button onClick={() => Navigate("/login")}>Login to Order</button>
+        )}
       </p>
     </div>
   );
